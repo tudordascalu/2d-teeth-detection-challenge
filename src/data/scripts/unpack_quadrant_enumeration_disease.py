107002d33ec50c69e0dad2eff3af207969fb9fe6
Tudor Dascalu <tudordascalu1@gmail.com>
2023-07-18 18:15:40 +0200

    Fix unpacking function

--- src/data/scripts/unpack_quadrant_enumeration_disease.py
+++ src/data/scripts/unpack_quadrant_enumeration_disease.py
@@ -1,17 +1,16 @@
-import numpy as np
-import torch
+import json
 
 if __name__ == "__main__":
     # Load data
-    data = np.load("data/processed/y_quadrant_enumeration_disease.npy", allow_pickle=True)
+    with open("data/processed/train_quadrant_enumeration_disease_healthy.json") as f:
+        data = json.load(f)
 
     # Unpack data
     data_unpacked = []
     for sample in data:
         for annotation in sample["annotations"]:
-            if torch.is_tensor(annotation["category_id_3"]):
-                annotation["category_id_3"] = annotation["category_id_3"].item()
             data_unpacked.append(dict(file_name=sample["file_name"], annotation=annotation))
 
     # Save data
-    np.save("data/processed/y_quadrant_enumeration_disease_unpacked.npy", data_unpacked)
+    with open("data/processed/train_quadrant_enumeration_disease_healthy_unpacked.json", "w") as f:
+        json.dump(data_unpacked, f, indent=4)
