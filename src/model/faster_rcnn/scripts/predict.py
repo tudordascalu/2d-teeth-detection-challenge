f8302d172d6311663175f267cefd270120faa380
Tudor Dascalu <tudordascalu1@gmail.com>
2023-07-18 17:56:06 +0200

    Refactors preiction script

--- src/model/faster_rcnn/scripts/predict.py
+++ src/model/faster_rcnn/scripts/predict.py
@@ -1,32 +1,90 @@
+import json
+
 import numpy as np
 import torch
-import yaml
-from pytorch_lightning import Trainer
-from torch.utils.data import DataLoader
+from torchvision.io import read_image
+from torchvision.ops import box_iou
+from tqdm import tqdm
 
-from src.data.panoramic_dataset import PanoramicDataset
 from src.model.faster_rcnn.faster_rcnn import FasterRCNN
+from src.utils.label_encoder import LabelEncoder
+from src.utils.processors import UniqueClassNMSProcessor
+
+
+class FilterOverlappingBoxes:
+    def __init__(self, iou_threshold=.75):
+        self.iou_threshold = iou_threshold
+        pass
+
+    def __call__(self, boxes1, boxes2):
+        """
+        Returns indices of non overlapping between boxes_1, boxes_2 corresponding to boxes 1.
+        """
+        # Compute the IoU matrix
+        iou_matrix = box_iou(boxes1, boxes2)
+
+        # Find boxes in the first list that overlap with any box in the second list
+        non_overlapping_indices = torch.all(iou_matrix < self.iou_threshold, dim=1).nonzero().squeeze().tolist()
+
+        # Return indices of overlapping boxes as a list
+        return non_overlapping_indices
+
 
 if __name__ == "__main__":
-    with open("./src/model/faster_rcnn/scripts/config.yml", "r") as f:
-        config = yaml.safe_load(f)
-    checkpoint = dict(version="version_3", model="epoch=epoch=88-val_loss=val_loss=0.81.ckpt")
-    # Find out whether gpu is available
+    # GPU or CPU
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
     # Load test split
-    y_test = np.load(f"data/final/y_{config['data_type']}_test.npy", allow_pickle=True)
-    dataset_args = dict(image_dir=f"{config['image_dir']}/{config['data_type']}/xrays", data_type=config["data_type"])
-    dataset_test = PanoramicDataset(y_test, **dataset_args)
-    # Define dataloader
-    loader_args = dict(batch_size=config["batch_size"], num_workers=0, pin_memory=True,
-                       collate_fn=PanoramicDataset.collate_fn)
-    loader_test = DataLoader(dataset_test, **loader_args)
-    # Define model
+    with open("data/processed/train_quadrant_enumeration_disease.json") as f:
+        X = json.load(f)
+
+    # Model
     model = FasterRCNN.load_from_checkpoint(
-        f"checkpoints/faster_rcnn/{checkpoint['version']}/checkpoints/{checkpoint['model']}")
-    model.hparams.config["output_path"] = f"output/{checkpoint['version']}__{checkpoint['model'].split('.')[0]}"
-    if device.type == "cpu":
-        trainer = Trainer()
-    else:
-        trainer = Trainer(accelerator="gpu", strategy="ddp", devices=1)
-    trainer.predict(model, dataloaders=loader_test)
+        "checkpoints/faster_rcnn/version_3/checkpoints/epoch=epoch=88-val_loss=val_loss=0.81.ckpt")
+    model.eval()
+
+    # Helpers
+    unique_class_nms_processor = UniqueClassNMSProcessor(iou_threshold=.75)
+    filter_overlapping_boxes = FilterOverlappingBoxes(iou_threshold=.5)
+    encoder = LabelEncoder()
+
+    for sample in tqdm(X, total=len(X)):
+        # Load image
+        file_name = sample["file_name"]
+        image = read_image(f"data/raw/training_data/quadrant_enumeration_disease/xrays/{file_name}").type(torch.float32)
+        image = image[0, ...].unsqueeze(0) / 255
+
+        # Predict
+        with torch.no_grad():
+            predictions = model(image.unsqueeze(0))[0]
+        predictions = unique_class_nms_processor(predictions)
+
+        # Extract data from predictions
+        predicted_scores = predictions["scores"]
+        predicted_boxes = torch.stack(predictions["boxes"])
+        predicted_labels = predictions["labels"]
+        predicted_labels_decoded = encoder.inverse_transform(predicted_labels)
+        predicted_teeth = [int((label - 1) % 10) for label in predicted_labels_decoded]
+        predicted_quadrants = [int((label - 1) / 10) for label in predicted_labels_decoded]
+        predicted_annotations = []
+        for box, score, label, tooth, quadrant in zip(predicted_boxes, predicted_scores, predicted_labels,
+                                                      predicted_teeth, predicted_quadrants):
+            predicted_annotations.append(
+                dict(bbox=box.tolist(),
+                     score=score.item(),
+                     label=label,
+                     ategory_id_1=quadrant,
+                     category_id_2=tooth,
+                     category_id_3=4,
+                     category_id_3_list=[4]))
+
+        # Remove annotations overlapping with existing annotations
+        boxes = torch.tensor([annotation["bbox"] for annotation in sample["annotations"]])
+        if len(boxes) > 0:
+            indices_non_overlapping = filter_overlapping_boxes(predicted_boxes, boxes)
+            predicted_annotations = np.array(predicted_annotations)[indices_non_overlapping].tolist()
+
+        sample["annotations"].extend(predicted_annotations)
+
+    with open("data/processed/train_quadrant_enumeration_disease_healthy.json", 'w') as f:
+        json.dump(X, f, indent=4)
